0ecf46044b14070f13620e9aa999ba29ea79c48b
Daniel Huang <36554153+lordChipotle@users.noreply.github.com>
2021-07-18 11:08:28 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/file/d/10gukwMih5ob2ojQNxb_RCKBa6dvg4b3f/view?usp=sharing'
+export_file_name = 'Hotdogclassifier.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['Hot_dog','Not_Hot_dog']
 path = Path(__file__).parent
 
 app = Starlette()
